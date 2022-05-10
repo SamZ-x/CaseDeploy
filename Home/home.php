6951715b493b6495e5d35bc32914434093ebf397
Xiaobin Zhu <sam.zhu992@gmail.com>
2022-05-10 15:20:08 -0600

    create new homepage for access all future cases

---
+++ Home/home.php
@@ -0,0 +1,27 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home page</title>
+    <link rel="stylesheet" href="homestyle.css">
+</head>
+<body class="homesite">
+    <div class="head">
+        <h1>CaseDeploy</h1>
+    </div>
+    <div class="content">
+    <a href="../Testcase01/index.php">Testcase01</a>
+    </div>
+    <footer>
+        &#174; Xiaobin Zhu <br>
+        <script>document.write('Last Modified: '+document.lastModified);</script>
+    </footer>
+</body>
+</html>
