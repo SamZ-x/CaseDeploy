e9e2929a0929d8fbbeea1d2b13881427d782418f
Xiaobin Zhu <sam.zhu992@gmail.com>
2022-06-03 08:37:33 -0600

    -u

--- SCI/index.php
+++ SCI/index.php
@@ -9,6 +9,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Serial Communication Interface</title>
+    <link rel="stylesheet" href="style_pj1.css">
 </head>
 <body class="homesite">
     <div>
