c8808d28915df625a1c5678324b02570cb3629ff
Xiaobin Zhu <sam.zhu992@gmail.com>
2022-06-15 12:46:01 -0600

    -u

--- SCI/index.php
+++ SCI/index.php
@@ -1,3 +1,33 @@
+<?php
+    //check the $_GET parameter
+    if(!empty($_GET['filename']))
+    {
+        $filename = basename($_GET['filename']);
+        $filepath = '../Files/'.$filename;
+        //check the filename and the file existence
+        if(!empty($filename) && file_exists($filepath))
+        {
+            //define headers before send file
+            header("Cache-Control: public; max-age=3600");                     //set cacha, hold for 1hour
+            header("Content-Description: File Transfer");               
+            header("Content-Disposition: attachment; filename = $filename");
+            header("Content-Type: application/zip");
+            header("Content-Transfer-Encoding: binary"); 
+
+            readfile($filepath);
+            
+            //redirection to index page
+            header("Location: index.php");
+            die();
+        }
+        //if filename error or no exist file, return error message
+        {
+            alert("Download Error! Please try later.");
+        }
+    }
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -22,7 +52,7 @@
                     <nav>
                         <ul class="navlist">
                             <li><a class="buttonlink" href="index.php">Introduction</a></li>
-                            <li><a class="buttonlink" href="download.php?filename=win-sci.zip">Download</a></li>
+                            <li><a class="buttonlink" href="index.php?filename=win-sci.zip">Download</a></li>
                             <li><a class="buttonlink" href="https://github.com/SamZ-x/SCI_9S12.git">Github</a></li>
                             <li><a class="buttonlink" href="http://www.casedeploy.com">Home</a></li>
                         </ul>
