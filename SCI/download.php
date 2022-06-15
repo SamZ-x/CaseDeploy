003b4d34dd77a0a50755c133e6fb8fb5e8adab7e
Xiaobin Zhu <sam.zhu992@gmail.com>
2022-06-15 12:44:14 -0600

    -de

--- SCI/download.php
+++ SCI/download.php
@@ -7,22 +7,22 @@
         error_log($filename);
         error_log($filepath);
         //check the filename and the file existence
-        $if(!empty($filename) && file_exists($filepath))
+        if(!empty($filename) && file_exists($filepath))
         {
             error_log("in");
 
-            // //define headers before send file
-            // header("Cache-Control: public; max-age=3600");                     //set cacha, hold for 1hour
-            // header("Content-Description: File Transfer");               
-            // header("Content-Disposition: attachment; filename = $filename");
-            // header("Content-Type: application/zip");
-            // header("Content-Transfer-Encoding: binary"); 
+            //define headers before send file
+            header("Cache-Control: public; max-age=3600");                     //set cacha, hold for 1hour
+            header("Content-Description: File Transfer");               
+            header("Content-Disposition: attachment; filename = $filename");
+            header("Content-Type: application/zip");
+            header("Content-Transfer-Encoding: binary"); 
 
-            // readfile($filepath);
+            readfile($filepath);
             
-            // //redirection to index page
-            // header("Location: index.php");
-            // die();
+            //redirection to index page
+            header("Location: index.php");
+            die();
         }
         //if filename error or no exist file, return error message
         {
