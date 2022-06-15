8a89e9ee228146a5baa85b2597bc1caf2bc74ea3
Xiaobin Zhu <sam.zhu992@gmail.com>
2022-06-15 12:43:10 -0600

    -de

--- SCI/download.php
+++ SCI/download.php
@@ -6,29 +6,29 @@
         $filepath = '../Files/'.$filename;
         error_log($filename);
         error_log($filepath);
-        // //check the filename and the file existence
-        // $if(!empty($filename) && file_exists($filepath))
-        // {
-        //     error_log("in");
+        //check the filename and the file existence
+        $if(!empty($filename) && file_exists($filepath))
+        {
+            error_log("in");
 
-        //     //define headers before send file
-        //     header("Cache-Control: public; max-age=3600");                     //set cacha, hold for 1hour
-        //     header("Content-Description: File Transfer");               
-        //     header("Content-Disposition: attachment; filename = $filename");
-        //     header("Content-Type: application/zip");
-        //     header("Content-Transfer-Encoding: binary"); 
+            // //define headers before send file
+            // header("Cache-Control: public; max-age=3600");                     //set cacha, hold for 1hour
+            // header("Content-Description: File Transfer");               
+            // header("Content-Disposition: attachment; filename = $filename");
+            // header("Content-Type: application/zip");
+            // header("Content-Transfer-Encoding: binary"); 
 
-        //     readfile($filepath);
+            // readfile($filepath);
             
-        //     //redirection to index page
-        //     header("Location: index.php");
-        //     die();
-        // }
-        // //if filename error or no exist file, return error message
-        // {
-        //     error_log("error");
-        //     alert("Download Error! Please try later.");
-        // }
+            // //redirection to index page
+            // header("Location: index.php");
+            // die();
+        }
+        //if filename error or no exist file, return error message
+        {
+            error_log("error");
+            alert("Download Error! Please try later.");
+        }
     }
 
     error_log("pass");
