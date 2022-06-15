67aa98749b867f8e7c43d171b7ff77130a974888
Xiaobin Zhu <sam.zhu992@gmail.com>
2022-06-15 12:45:56 -0600

    -u

--- SCI/download.php
+++ SCI/download.php
@@ -4,13 +4,9 @@
     {
         $filename = basename($_GET['filename']);
         $filepath = '../Files/'.$filename;
-        error_log($filename);
-        error_log($filepath);
         //check the filename and the file existence
         if(!empty($filename) && file_exists($filepath))
         {
-            error_log("in");
-
             //define headers before send file
             header("Cache-Control: public; max-age=3600");                     //set cacha, hold for 1hour
             header("Content-Description: File Transfer");               
@@ -26,10 +22,7 @@
         }
         //if filename error or no exist file, return error message
         {
-            error_log("error");
             alert("Download Error! Please try later.");
         }
     }
-
-    error_log("pass");
 ?>
