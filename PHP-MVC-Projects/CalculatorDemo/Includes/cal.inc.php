003a083c44cff5ec915eb109987dfef87ae2e4eb
Xiaobin Zhu <sam.zhu992@gmail.com>
2022-06-18 09:41:29 -0600

    -t

--- PHP-MVC-Projects/CalculatorDemo/Includes/cal.inc.php
+++ PHP-MVC-Projects/CalculatorDemo/Includes/cal.inc.php
@@ -5,13 +5,27 @@
 
     //retrieve the parameters
     $operator = $_POST['operator'];
-    $num1 = $_POST['num1'];
-    $num2 = $_POST['num2'];
+    $num1 = intval($_POST['num1']);
+    $num2 = intval($_POST['num2']);
 
     //create calculation object
-    $simplyCalculation = new Calculation($operator, $num1, $num2);
+    $simplyCalculation = new Calc($operator, $num1, $num2);
 
     //get result
     echo $simplyCalculation->GetResult();
+    try {
+        $result = $simplyCalculation->GetResult();
+        
+        {
+            header("Location: ../Views/index.php?result={$result}");  
+            die();
+        }
+
+    } catch (TypeError $e) {
+        {
+            header("Location: ../Views/index.php?result={$e->message}");  
+            die();
+        }
+    }
 
 ?>
