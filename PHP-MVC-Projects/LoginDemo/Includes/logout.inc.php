01282a3134258bc0128c05559588b43e11319a6b
Xiaobin Zhu <sam.zhu992@gmail.com>
2022-06-22 09:34:04 -0600

    -f

--- PHP-MVC-Projects/LoginDemo/Includes/logout.inc.php
+++ PHP-MVC-Projects/LoginDemo/Includes/logout.inc.php
@@ -4,6 +4,7 @@
     //distroy the current seesion
     if(isset($_SESSION["userid"]))
     {
+        session_start();
         session_unset();
         session_destroy();
         header("location: ../Views/login.php?error=logout");
