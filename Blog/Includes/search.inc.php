bbbbd62f7682cf1f81eb79b59067fae740984fb5
Xiaobin Zhu <sam.zhu992@gmail.com>
2022-06-23 00:26:32 -0600

    -a build blog index page

---
+++ Blog/Includes/search.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+    //bridge between front-end and back-end
+
+    //submit button checck
+    if(isset($_GET['submit']))
+    {
+        //get the search info
+        $category = $_GET['searchCategory'];
+        $keyword = $_GET['searchInfo'];
+
+        //include class file and Instantiate searchcontr
+
+
+        //run error handle and search data
+    }
