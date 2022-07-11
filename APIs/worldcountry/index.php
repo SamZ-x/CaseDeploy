bfb6a21249e8f9f7dc2ebdafad5000b2fea32409
Xiaobin Zhu <sam.zhu992@gmail.com>
2022-07-11 11:25:10 -0600

    -fix api home page

--- APIs/worldcountry/index.php
+++ APIs/worldcountry/index.php
@@ -1,5 +1,5 @@
 <?php
-    require_once "Views/header.view.php";
-    require_once "Views/main.view.php";
-    require_once "Views/footer.view.php";
+    require_once "views/header.view.php";
+    require_once "views/main.view.php";
+    require_once "views/footer.view.php";
 ?>
