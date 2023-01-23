7e40fe3d37a393b4d08ee9b30b98c3078731988c
Xiaobin Zhu <sam.zhu992@gmail.com>
2023-01-23 08:42:16 -0700

    switch to deploy mode

--- webapp/Blog/Models/Dbh.class.php
+++ webapp/Blog/Models/Dbh.class.php
@@ -15,19 +15,19 @@
                 //get the database connection credential for external json file
                 $DBcredential = json_decode(file_get_contents('../Dbh.json', true), true);
                 
-                ////////// TEST //////////////
-                $host = "localhost";
-                $user = "test";
-                $pwd = "test";
-                $dbName = "blog";
-                /////////////////////////////
+                // ////////// TEST //////////////
+                // $host = "localhost";
+                // $user = "test";
+                // $pwd = "test";
+                // $dbName = "blog";
+                // /////////////////////////////
 
-                //////// DEPLOY //////////////
-                // $host = $DBcredential['host'];
-                // $user = $DBcredential['user'];
-                // $pwd = $DBcredential['pwd'];
-                // $dbName = $DBcredential['dbName'];
-                ///////////////////////////
+                ////// DEPLOY //////////////
+                $host = $DBcredential['host'];
+                $user = $DBcredential['user'];
+                $pwd = $DBcredential['pwd'];
+                $dbName = $DBcredential['dbName'];
+                /////////////////////////
                 
                 //database server name
                 $dsn = 'mysql:host='.$host.';dbname='.$dbName;
